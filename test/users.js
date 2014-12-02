c6939f2b520e0a3358922a1f327117e5b2ee9149
Simon Bugert <simon.bugert@googlemail.com>
2014-12-02 20:32:34 +0000

    Foundation for tests

---
+++ test/users.js
@@ -0,0 +1,10 @@
+var Code = require('code')
+  , Lab = require('lab')
+  , server = require('../shop').server;
+
+var lab = exports.lab = Lab.script();
+
+lab.experiment('First test', function() {
+  // test here
+});
+
