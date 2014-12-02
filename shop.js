5fd04b15d6e1ae2e8402f6526b04a493ab7fb3a2
Simon Bugert <simon.bugert@googlemail.com>
2014-12-02 20:30:21 +0000

    Foundation for tests

--- shop.js
+++ shop.js
@@ -5,8 +5,14 @@ var server = new Hapi.Server(options.server);
 
 server.connection(options.connection);
 
-server.start(function (err) {
-  if (err) throw err;
-  console.log('Server started at: ' + server.info.uri);
-});
+if (!module.parent) {
+  server.start(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log('Server started at: ' + server.info.uri);
+  });
+}
+
+module.exports.server = server;
 
