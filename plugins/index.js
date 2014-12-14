5c99c6a8c08151906034805be25e635efbd1038f
Simon Bugert <simon.bugert@googlemail.com>
2014-12-14 20:46:46 +0000

    Make justify plugin configurable

--- plugins/index.js
+++ plugins/index.js
@@ -1,11 +1,26 @@
+var options = require('../options').plugins;
+
 var plugins = [
   {
     register: require('hapi-auth-cookie')
   },
   {
-    register: require('./hapi-justify')
+    register: require('./hapi-justify'),
+    options: options.justify
   }
 ];
+
+if(process.env.NODE_ENV !== 'PRODUCTION') {
+  plugins = plugins.concat([
+    {
+      register: require('tv'),
+      options: {
+        endpoint: '/debug/console',
+        queryKey: 'debug'
+      }
+    }
+  ]);
+}
 
 module.exports = function (server) {
   server.register(plugins, function (err) {
