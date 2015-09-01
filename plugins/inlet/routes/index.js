3dcb19f5944b3e012896fe7b95b029f85c243e01
Simon Bugert <simon.bugert@googlemail.com>
2015-09-01 21:35:48 +0000

    WIP

--- plugins/inlet/routes/index.js
+++ plugins/inlet/routes/index.js
@@ -8,31 +8,31 @@ module.exports = [
       //auth: 'session'
     }
   },
-  //{
-    //method: 'GET',
-    //path: '/inlet/newprod',
-    //config: {
-      //handler: require('../handlers/newprod').get,
-      ////validate: require('../validation/product').get,
-      ////auth: 'session'
-    //}
-  //},
-  //{
-    //method: 'GET',
-    //path: '/inlet/highprod',
-    //config: {
-      //handler: require('../handlers/highprod').get,
-      ////validate: require('../validation/product').get,
-      ////auth: 'session'
-    //}
-  //},
-  //{
-    //method: 'GET',
-    //path: '/inlet/cateprod',
-    //config: {
-      //handler: require('../handlers/cateprod').get,
-      ////validate: require('../validation/product').get,
-      ////auth: 'session'
-    //}
-  //}
+  {
+    method: 'GET',
+    path: '/inlet/newprod',
+    config: {
+      handler: require('../handlers/newprod').get,
+      //validate: require('../validation/product').get,
+      //auth: 'session'
+    }
+  },
+  {
+    method: 'GET',
+    path: '/inlet/highprod',
+    config: {
+      handler: require('../handlers/highprod').get,
+      //validate: require('../validation/product').get,
+      //auth: 'session'
+    }
+  },
+  {
+    method: 'GET',
+    path: '/inlet/cateprod',
+    config: {
+      handler: require('../handlers/cateprod').get,
+      //validate: require('../validation/product').get,
+      //auth: 'session'
+    }
+  }
 ]
